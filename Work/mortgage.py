6e0e69336ecbb8f94f57a29ed8fc182787deda99
David White <white@uw.edu>
2020-06-16 18:57:17 -0400

    1.3 Exercises

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,28 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.00
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+
+month = 1
+extra_payment_start_month = 49 # first extra payment in year 5
+extra_payment_end_month = 49+48-1 # last extra payment 48 months later
+extra_payment = 1000
+
+while principal > 0:
+    principal = principal * (1 + rate/12)
+    if principal < payment:
+        payment = principal
+    principal = principal - payment
+    if extra_payment_start_month <= month and month <= extra_payment_end_month:
+        principal -= extra_payment
+        total_paid += extra_payment
+    total_paid = total_paid + payment
+    print(month, round(total_paid, 2), round(principal, 2))
+    month += 1
+
+print('Total paid', total_paid)
+
