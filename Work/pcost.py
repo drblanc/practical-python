60dc6a5afe067fffe7db3149869d4ea89c34822a
David White <white@uw.edu>
2020-06-17 15:39:40 -0400

    Exercise 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,21 +1,26 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.32
+
+import csv
 
 def portfolio_cost(filename):
     total_cost = 0
 
-    with open(filename, 'rt') as f:
-        headers = next(f)
-        for line in f:
-            row = line.split(',')
-            try:
-                nshares = int(row[1])
-                sharecost = float(row[2])
-            except ValueError:
-                print('Cannot parse:', line)
-                nshares = 0
-            total_cost = total_cost + nshares * sharecost
+    f = open(filename)
+    rows = csv.reader(f)
+
+    headers = next(rows)
+    for row in rows:
+        try:
+            nshares = int(row[1])
+            sharecost = float(row[2])
+        except ValueError:
+            print('Cannot parse:', row)
+            nshares = 0
+        total_cost = total_cost + nshares * sharecost
+
+    f.close()
     return total_cost
 
 cost = portfolio_cost('Data/portfolio.csv')
