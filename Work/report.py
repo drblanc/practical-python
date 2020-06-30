689b14c1977a62725242cea13f3d79d5e9e753b5
David White <white@uw.edu>
2020-06-30 13:53:18 -0400

    Exercise 2.7 (formatting?)

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,6 @@
 # report.py
 #
-# Exercise 2.4
+# Exercise 2.4-2.7
 
 import csv
 
@@ -17,4 +17,32 @@ def read_portfolio(filename):
             portfolio.append({'name':row[0], 'shares':nshares, 'price':price})
     return portfolio
 
+def read_prices(filename):
+    '''Reads csv of name,price into a dict'''
+    prices = {}
 
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                price = float(row[1])
+                prices[row[0]] = price
+            except:
+                pass
+    return prices
+
+portfolio = read_portfolio('Data/portfolio.csv')
+prices = read_prices('Data/prices.csv')
+
+portfolio_value = 0.0
+for h in portfolio:
+    portfolio_value += h['shares']*h['price']
+
+print(f'Original value:{portfolio_value:10.2f}')
+
+current_value = 0.0
+for h in portfolio:
+    current_value += h['shares']*prices[h['name']]
+
+print(f'Current value: {current_value:10.2f}')
+print('Gain: %10.2f' % (current_value - portfolio_value))
