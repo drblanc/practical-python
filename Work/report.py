ac263a591752e73da2da4a29d48104eeb0e17aa4
David White <white@uw.edu>
2020-06-30 13:22:52 -0400

    Exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,20 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+def read_portfolio(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            nshares = int(row[1])
+            price = float(row[2])
+            portfolio.append((row[0], nshares, price))
+    return portfolio
+
+
